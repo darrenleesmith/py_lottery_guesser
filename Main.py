ddc88aa0fbd4682b97c1d182f4388fe3a09816f4
darrenleesmith <48777290+darrenleesmith@users.noreply.github.com>
2020-05-10 08:32:40 +0100

    Update Main.py
    added option to retry

--- Main.py
+++ Main.py
@@ -1,25 +1,37 @@
 import random
-import os
 
 daList = []
 
 print("Number Generator V0.1\n")
 
-userSel = int(input("Please select a number of numbers you would like: "))
+userRetry = bool(1)
 
-def rangen():
-    rannum = random.randrange(1, 50)
-    #print(rannum)
-    if daList.count(rannum):
-        #print('Double found')
+while(userRetry == 1):
+
+    userSel = int(input("Please select a number of numbers you would like: "))
+
+    def rangen():
+        rannum = random.randrange(1, 50)
+        #print(rannum)
+        if daList.count(rannum):
+            #print('Double found')
+            return
+        daList.append(rannum)
+        daList.sort()
         return
-    daList.append(rannum)
-    daList.sort()
-    return
     
-while(len(daList) < userSel):
-    rangen()
+    while(len(daList) < userSel):
+        rangen()
     
-os.system('clear')
+    print("\nLucky numbers: ", daList)
+    
+    userCho = input("\nWould you like to try again? (yes or no) ")
+    if userCho == ("yes"):
+        daList.clear()
+        userRetry = bool(1)
+        
+    if userCho == ("no"):
+        userRety = bool(0)
+        break
 
-print("\nLucky numbers: ", daList)
+print("\nEnd")
