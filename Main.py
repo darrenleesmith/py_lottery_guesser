7253b078822c696fe1ca22807a4a6f240bcb6580
darrenleesmith <48777290+darrenleesmith@users.noreply.github.com>
2020-05-10 04:17:15 +0100

    Update Main.py
    Added user options etc

--- Main.py
+++ Main.py
@@ -1,17 +1,25 @@
 import random
+import os
+
 daList = []
+
+print("Number Generator V0.1\n")
+
+userSel = int(input("Please select a number of numbers you would like: "))
 
 def rangen():
     rannum = random.randrange(1, 50)
     #print(rannum)
     if daList.count(rannum):
-        #print('Double')
+        #print('Double found')
         return
     daList.append(rannum)
     daList.sort()
     return
     
-while(len(daList) < 7):
+while(len(daList) < userSel):
     rangen()
+    
+os.system('clear')
 
-print(daList)
+print("\nLucky numbers: ", daList)
